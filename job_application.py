f6171f3cfff01dce03d39e9ad8b73e82c8b61f09
Tempestt <66889976+tempsthoughts@users.noreply.github.com>
2020-09-06 17:23:22 -0500

    Add files via upload

---
+++ job_application.py
@@ -0,0 +1,33 @@
+# This job application will ask applicants basic questions about them and their background.
+
+# Question 1
+name =  input('What is your first and last name? ')
+
+# Question 2
+address = input('What is your current mailing address? ')
+
+# Question 3
+city_state_zip = input('City, State, ZIP Code ')
+
+# Question 4
+phone_number = int(input('Your phone number including area code. '))
+
+# Question 5
+current_employer = input('What is your current employers name? ')
+
+# Question 6
+current_position = input('What is your current job title? ')
+
+# Question 7
+job_description = input('Give a brief description of your job duties ')
+
+# Question 8
+employment_dates = input('List your start date in mm/yy format ')
+
+# Question 9
+short_answer = input('Why do you want this position? ')
+
+# Question 10
+education = input('List your education background. Ex. Degree, bootcamp graduate, self-taught. ')
+
+
